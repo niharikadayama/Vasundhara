f420c7d98c61e8570a98fb8905635127e2caebd9
Rutvik P. Pohankar <pohankarrutvik@gmail.com>
2020-10-11 19:26:55 +0530

    Explore page updated

---
+++ explore.php
@@ -0,0 +1,61 @@
+<html>
+
+<head>
+    <title>Explore_Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+
+<body>
+    <!-- <img src="Homepage_bg.png" alt="HTML5 Icon"> -->
+
+    <div class="logo">
+        <a href="#"><img src="Vasundhara2(black bg).png" width="298" height="73"></a>
+    </div>
+    <a href="explore.php">
+        <div class="card">
+
+            <div class="card-image"></div>
+            <div class="card-text">
+
+                <span class="date">Flower product</span>
+                <h2>Gulmehar NGO</h2>
+                <p>Artisans recycling floral waste into natural dyes</p>
+            </div>
+            <div class="card-stats2">
+                <p>“Life is the flower for which love is the honey.” – Victor Hugo</p>
+            </div>
+
+
+
+        </div>
+    </a>
+    <div class="card">
+        <div class="card-image2"></div>
+        <div class="card-text">
+            <span class="date">Flower product</span>
+            <h2>ADIV Pure Nature</h2>
+            <p>Artisans recycling floral waste into natural dyes</p>
+        </div>
+        <div class="card-stats">
+            <p>“There is something of the marvelous in all things of nature" -Aristotle</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-image3"></div>
+        <div class="card-text">
+            <span class="date">Plastic waste product</span>
+            <h2>Shayna ECO unified</h2>
+            <p>Innovative conversion of waste plastic into designer and durable tiles</p>
+        </div>
+        <div class="card-stats3">
+            <p>“The world is mud-luscious and puddle-wonderful" e.e. cummings</p>
+        </div>
+    </div>
+
+
+
+</body>
+
+
+</html>
